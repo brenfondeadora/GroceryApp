b2048f482bf3784ddc472a50b3f619509b307baa
Brenda SC <brenda.saavedra@fondeadora.com>
2023-05-18 15:42:42 -0600

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Brenda Saavedra  on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
